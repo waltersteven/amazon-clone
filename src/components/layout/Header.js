2ae1654f660f668c9870b543983df134b089e205
Walter Pariona <wltrst97@gmail.com>
2020-09-20 22:33:47 -0500

    Added material-ui for header icons

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import { ShoppingBasket } from '@material-ui/icons';
 
 const Header = () => {
   return (
     <div className='header'>
       <img className='header__logo' src='amazon-logo.png' alt='Amazon Logo' />
+
       <div className='header__search'>
         <input type='text' className='header__searchInput' />
-        {/* Logo */}
+        <SearchIcon className='header__searchIcon' />
       </div>
 
       <div className='header__nav'>
         <div className='header__option'>
           <span className='header__optionLineOne'>Hello Walter!</span>
-          <span className='header__optionLineOne'>Sign In</span>
+          <span className='header__optionLineTwo'>Sign In</span>
         </div>
         <div className='header__option'>
           <span className='header__optionLineOne'>Returns</span>
-          <span className='header__optionLineOne'>& Orders</span>
+          <span className='header__optionLineTwo'>& Orders</span>
         </div>
         <div className='header__option'>
           <span className='header__optionLineOne'>Your</span>
-          <span className='header__optionLineOne'>Prime</span>
+          <span className='header__optionLineTwo'>Prime</span>
+        </div>
+
+        <div className='header__optionBasket'>
+          <ShoppingBasket />
+          <span className='header__optionLineTwo header__basketCount'>0</span>
         </div>
       </div>
     </div>
