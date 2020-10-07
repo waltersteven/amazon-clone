8ed09200d2bb770e24848943082a3f2f2c842663
Walter Pariona <wltrst97@gmail.com>
2020-10-06 23:55:44 -0500

    Added styles to login component

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='login'>
       <Link to='/'>
@@ -13,19 +24,23 @@ function Login() {
         <h1>Sign-in</h1>
         <form action=''>
           <h5>E-mail</h5>
-          <input type='text' />
+          <input type='text' value={email} onChange={(e) => setEmail(e.target.value)} />
 
           <h5>Password</h5>
-          <input type='password' />
+          <input type='password' value={(e) => setPassword(e.target.value)} />
 
-          <button>Sign In</button>
+          <button className='login__signInButton' type='submit' onClick={signIn}>
+            Sign In
+          </button>
         </form>
 
         <p>
           By signing-in you agree to the AMAZON CLONE Conditions of Use & Sale. Please see our
-          Privacy Note, our Cookies Notice and our Interest-based Ads Notice
-          <button>Create your Amazon Account</button>
+          Privacy Note, our Cookies Notice and our Interest-based Ads Notice.
         </p>
+        <button onClick={register} className='login__registerButton'>
+          Create your Amazon Account
+        </button>
       </div>
     </div>
   );
