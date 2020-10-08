fe5e4189e0423db1d5262b02400c00905deb76f3
Walter Pariona <wltrst97@gmail.com>
2020-10-07 22:37:09 -0500

    Added user email to checkout page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Home from './components/home/Home';
 import Checkout from './components/checkout/Checkout';
 import Login from './components/login/Login';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+    auth.onAuthStateChanged((authUser) => {
+      console.log('User is >>>', authUser);
+
+      if (authUser) {
+        // The user just logged in / the user was logged in
+        dispatch({
+          type: 'SET_USER',
+          user: authUser,
+        });
+      } else {
+        // The user is logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <div className='app'>
--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -5,7 +5,7 @@ import './Checkout.css';
 import CheckoutProduct from './CheckoutProduct';
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
 
   return (
     <div className='checkout'>
@@ -16,6 +16,7 @@ function Checkout() {
           alt=''
         />
         <div>
+          <h3>Hello, {user?.email}</h3>
           <h2 className='checkout__title'>Your Shopping Basket</h2>{' '}
           {basket.map((item) => (
             <CheckoutProduct
