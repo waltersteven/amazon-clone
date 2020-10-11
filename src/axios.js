36563b1da3394824a3e4d9a4be0d923172abdd5d
Walter Pariona <wltrst97@gmail.com>
2020-10-10 20:20:26 -0500

    changed api base url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/clone-f3966/us-central1/api',
+  baseURL: 'https://us-central1-clone-f3966.cloudfunctions.net/api',
+  // 'http://localhost:5001/clone-f3966/us-central1/api',
 });
 
 export default instance;
